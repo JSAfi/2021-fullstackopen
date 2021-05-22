2916dbd7bc6f4092a3e3d8d151fd108f26508075
JSAfi <jaakko.m.salmela@gmail.com>
2021-05-22 20:53:38 +0300

    Tehtävä 1.4 tehty

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -24,9 +24,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part = {props.p1}/>
-      <Part part = {props.p2}/>
-      <Part part = {props.p3}/>
+      <Part part = {props.parts[0]}/>
+      <Part part = {props.parts[1]}/>
+      <Part part = {props.parts[2]}/>
     </>
   )
 }
@@ -35,7 +35,7 @@ const Total = (props) => {
   return (
     <>
      <p>
-        Number of exercises {props.exerc1.exercises + props.exerc2.exercises + props.exerc3.exercises}
+        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
       </p>
     </>
   )
@@ -43,24 +43,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = {
+  const parts = [
+    {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content p1={part1} p2={part2} p3={part3}/>
-      <Total exerc1 = {part1} exerc2 = {part2} exerc3 = {part3}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
