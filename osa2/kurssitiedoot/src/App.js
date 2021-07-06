8a42b47583665136af89d0f04f435914acd6b132
JSAfi <jaakko.m.salmela@gmail.com>
2021-07-06 22:47:44 +0300

    tehtävä 2.4 done

--- osa2/kurssitiedoot/src/App.js
+++ osa2/kurssitiedoot/src/App.js
@@ -39,8 +39,8 @@ const Total = ({parts}) => {
 }
 
 const Course = ({course}) => {
-  return (
-    <div>
+    return (
+     <div>
       <Header course={course} />
       <Content parts = {course.parts} />
       <Total parts = {course.parts} />
@@ -48,31 +48,65 @@ const Course = ({course}) => {
     )
 }
 
+const Courses = ({courses}) => {
+  console.log({courses})
+  return (
+    <>
+      {courses.map((crs)=> (
+        <Course course={crs} key={crs.id} />
+      ))}
+    </>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (      
-    <Course course={course} />
+    <Courses courses={courses} />
   )
 }
 
