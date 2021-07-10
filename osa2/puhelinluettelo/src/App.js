af1efcf7f6e9cdb60ff6a3b75a18b93b278ede2b
JSAfi <jaakko.m.salmela@gmail.com>
2021-07-10 22:13:49 +0300

    nimien lisäys listaan

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Person = ({name}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>{name}</p>
+  )
 }
 
-export default App;
+const App = (props) => {
+  const [ persons, setPersons] = useState(props.persons) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleAddEntry = (event) =>{
+    event.preventDefault()
+    console.log('button clicked', event)
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddEntry}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <Person name={person.name} key={person.name} />
+      )}
+    </div>
+  )
+
+}
+
+export default App
