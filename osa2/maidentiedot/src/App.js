835a42172cf0d4765702fe7bec0a1d330118acb8
JSAfi <jaakko.m.salmela@gmail.com>
2021-07-18 18:28:39 +0300

    tehtävä 2.14 tehty mutta refaktorointi voisi olla pooikaa

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,6 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const Weather = ({city}) => {
+  const [weather, setWeather] = useState([])
+
+  const api_key = process.env.REACT_APP_API_KEY
+
+  console.log(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}`)
+
+  useEffect(() => {
+    axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+
+  console.log(api_key)
+  console.log(weather)
+
+  if(weather.length !== 0){
+    return (
+    <div>
+      <h3>{weather.current.condition.text}</h3>
+      <img src={weather.current.condition.icon} alt="" width="100"/>
+    </div>
+    )
+  } else {
+
+    return(
+      <div></div>
+    )
+  }
+}
+
 const Countries = ({countries, filter, handleClick}) => {
   console.log(countries.length)
   if(countries.length > 10) {
@@ -20,6 +53,7 @@ const Countries = ({countries, filter, handleClick}) => {
       )
     )
   }
+
   return (
     countries.map((country) => 
       <div key={country.name}>
@@ -44,6 +78,12 @@ const Countries = ({countries, filter, handleClick}) => {
         </div>
         <div>
           <img src={country.flag} width="500" alt="lipun kuva"/>
+        </div>
+        <h2>
+          Weather in {country.capital}
+        </h2>
+        <div>
+          <Weather city = {country.capital} />
         </div>
       </div>
     )
@@ -74,11 +114,11 @@ const App = () =>{
   }
 
   useEffect(() => {
-    console.log('effect')
+    console.log('country effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response =>{
-        console.log('promise fulfilled')
+        console.log('countrydata promise fulfilled')
         console.log(response.data)
         setAllCountries(response.data)
       })
