0bfdef2bb77a8745d94dc10c98909c57e9926e4c
JSAfi <jaakko.m.salmela@gmail.com>
2021-08-31 19:58:58 +0300

    tehtävä 4.4.

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const lkm = blogs.reduce((acc, curr) => {
+        return acc + curr['likes']
+    }, 0)
+
+    return lkm
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
