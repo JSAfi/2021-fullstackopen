bea86dd5014374c499451ad0d2e53b2c3577c8d9
JSAfi <jaakko.m.salmela@gmail.com>
2021-09-04 13:50:45 +0300

    4.5. done 4.6 alustettu osin

--- osa4/blogilista/tests/unittest.test.js
+++ osa4/blogilista/tests/unittest.test.js
@@ -24,11 +24,141 @@ describe('total likes', () => {
           url: 'http://www.google.com/finance',
           likes: 55,
           __v: 0
-        }
+        },
+        {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 55,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 0,
+            __v: 0
+          }
     ]
 
     test('when list has only one blog equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithSomeBlogs)
-        expect(result).toBe(60)
+        expect(result).toBe(115)
+    })
+})
+
+describe('favorite blog', () => {
+    const listWithSomeBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: '5a422aa71b54a676234d17f0',
+          title: 'Honkytonk Badonkadonks',
+          author: 'Mr. Dorkydork Dingledong',
+          url: 'http://www.google.com/finance',
+          likes: 55,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 155,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 0,
+            __v: 0
+          },
+          {
+            _id: '5b422bb71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 235,
+            __v: 0
+          }
+    ]
+
+    test('return the blog with most likes', () => {
+        const result = listHelper.favoriteBlog(listWithSomeBlogs)
+        const expected = {
+            _id: '5b422bb71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 235,
+            __v: 0
+          }
+
+        expect(result).toEqual(expected)
+    })
+})
+
+describe('author with most blogs', () => {
+    const listWithSomeBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: '5a422aa71b54a676234d17f0',
+          title: 'Honkytonk Badonkadonks',
+          author: 'Mr. Dorkydork Dingledong',
+          url: 'http://www.google.com/finance',
+          likes: 55,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 155,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'Mr. Dorkydork Dingledong',
+            url: 'http://www.google.com/finance',
+            likes: 0,
+            __v: 0
+          },
+          {
+            _id: '5b422bb71b54a676234d17f0',
+            title: 'Honkytonk Badonkadonks',
+            author: 'KingKong PingPong',
+            url: 'http://www.google.com/finance',
+            likes: 235,
+            __v: 0
+          }
+    ]
+
+    test('return the author with most blogs', () => {
+        const result = listHelper.favoriteBlog(listWithSomeBlogs)
+        const expected = {
+            author: 'Mr. Dorkydork Dingledong',
+            blogs: 3
+          }
+
+        expect(result).toEqual(expected)
     })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,7 +10,29 @@ const totalLikes = (blogs) => {
     return lkm
 }
 
+const favoriteBlog = (blogs) => {
+    let maxLikes = -1
+    let storedIndex = 0
+    let i = 0
+    for(const index of blogs.values()) {
+//        console.log('now at ', i, ' where there are ', index.likes, ' likes')
+        if (index.likes > maxLikes) {
+            maxLikes = index.likes
+            storedIndex = i
+        }
+        i++
+//        console.log('i\'ve got max likes at ', storedIndex, ' with ', maxLikes, ' likes')
+    }
+    return blogs[storedIndex]
+}
+
+const mostBlogs = (blogs) => {
+    return ""
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
