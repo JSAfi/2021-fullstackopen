d8ab73db6e4c9bf6abd8712037752f19f347f2a6
JSAfi <jaakko.m.salmela@gmail.com>
2021-09-19 18:29:37 +0300

    tehtävä 4.19 postaaminen token-pohjaisella autentikoinnilla tehty

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,32 +1,25 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
 const Blog = require('../models/blog')
 const User = require('../models/user')
-/*
-blogsRouter.get('/', (request, response) => {
-    Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-  })
-*/
-/*
-blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if(blog) {
-                response.json(blog.toJSON())
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-*/
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  console.log(authorization)
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs.map(blog => blog.toJSON()))
 })
+
+
 blogsRouter.get('/:id', async (request, response) => {
   const blogs = await Blog.findById(request.params.id).populate('user', {username: 1, name: 1})
   if(blogs) {
@@ -35,10 +28,23 @@ blogsRouter.get('/:id', async (request, response) => {
     response.status(404).end()
   }
 })
+
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if(!token || !decodedToken.id) {
+    console.log("---")
+    console.log("DECODDTOKEN ID", decodedToken.id)
+    console.log("TOKEN", token)
+    return response.status(401).json({error: 'token missing or invalid'})
+  }
 
+/*  
   const user = await User.findById(body.userId)
+*/
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     "title": body.title,
