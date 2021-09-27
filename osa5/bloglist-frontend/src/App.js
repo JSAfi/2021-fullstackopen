ab7f4f4f847c7222a409f0bccf42dbb5f9fbf33f
JSAfi <jaakko.m.salmela@gmail.com>
2021-09-27 19:18:21 +0300

    5.3 ja 5.4 tehty

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import Notification from './components/Notification'
 import Blog from './components/Blog'
 import loginService from './services/login'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [message, setMessage] = useState(null)
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -26,10 +31,10 @@ const App = () => {
       setPassword('')
       console.log('KIRJAUTUNUT USER ', user)
     } catch(exception) {
-/*      setErrorMessage('wrong credentials')
+      setMessage('wrong credentials')
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000) */
+        setMessage(null)
+      }, 5000) 
       console.log('wrong credentials try-catch block!')
     } 
   }
@@ -57,22 +62,49 @@ const App = () => {
       <button type="submit">login</button>
     </form>      
   )
-/*
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <input
-        value={newBlog}
-        onChange={handleBlogChange}
-      />
-      <button type="submit">save</button>
-    </form>  
-  )
-*/
+
+  const addBlog = async (event) => {
+    try {
+      const blogObject = {
+        title: newBlog,
+        author: author,
+        url: url
+      }
+      const response = blogService.create(blogObject)
+      console.log(response)
+
+      setNewBlog('')
+      setAuthor('')
+      setUrl('')
+      setMessage('Blogi lisätty!')
+      setTimeout(() => {
+        setMessage(null)
+      }, 10000) 
+    } catch (exception) {
+      console.log('poikkeus blogin lisäämisessä!')
+    }
+  } 
+
+  const handleAuthorChange = event => {
+    console.log(event.target.value)
+    setAuthor(event.target.value)
+  }
+
+  const handleBlogChange = event => {
+    console.log(event.target.value)
+    setNewBlog(event.target.value)
+  }
+  const handleUrlChange = event => {
+    console.log(event.target.value)
+    setUrl(event.target.value)
+  }
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
     if(loggedUserJSON) {
@@ -84,17 +116,37 @@ const App = () => {
 
   return (
     <div>
-      <div>{username}</div>
-      <div>{password}</div>
+      <Notification message={message} />
       { user === null ? 
         loginForm() :
         <div>
             <p>{user.name} logged in</p>
-        
+            <form onSubmit={addBlog}>
+        <div>
+        <input
+          value={newBlog}
+          onChange={handleBlogChange}
+        />
+        </div>
+        <div>
+        <input
+          value={author}
+          onChange={handleAuthorChange}
+        />
+        </div>
+        <div>
+        <input
+          value = {url}
+          onChange={handleUrlChange} />
+        </div>
+        <div>
+          <button type="submit">create</button>
+        </div>
+    </form> 
         <h2>blogs</h2>
         <ul>
           {blogs.map(blog =>
-            <li>
+            <li key={blog.id}>
               <Blog key={blog.id} blog={blog} />
             </li>
           )}
